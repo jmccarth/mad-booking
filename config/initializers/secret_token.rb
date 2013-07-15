a7eabc6377a417ca30054739105f77cc3e25db91
James McCarthy <jmccarth@uwaterloo.ca>
2013-07-15 15:59:00 -0400

    Initial commit of booking system

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Equip::Application.config.secret_token = '20c08f0bd4d92dbd0f09f2460fe30deacc18bef5eedef6b5d36d75746b5651985bc9f6307e1f214f0379d780dbe7dc93d695c4a7a996848dafceb238679f6483'
