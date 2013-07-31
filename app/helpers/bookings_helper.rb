ba56859cb77bae3f8c8d24849bc946de41a9d56c
James McCarthy <jmccarth@uwaterloo.ca>
2013-07-31 13:41:04 -0400

    Conflict check now works on update event

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,20 +1,6 @@
 module BookingsHelper
   
-  def has_conflict(booking, equip_ids)
-    conflicts = false
-    conflict_ids=[]
-    #given an array of equipment ids, check to see if there are conflicting events
-    @equip_conflicts = Booking.find(:all, :include=>:equipments, :conditions=>['equipment.id in (?)',equip_ids])
-    @equip_conflicts.each do |booking_test|
-      s = IceCube::Schedule.from_yaml(booking_test.schedule)
-      s1 = IceCube::Schedule.from_yaml(booking.schedule)
-      if s.conflicts_with?(s1) and booking_test.id != booking.id
-        conflicts = true
-        conflict_ids.push(booking_test.id)
-      end
-    end
-    conflict_ids
-  end
+
   
   def find_by_date_range(start_date,end_date)
     @all_bookings = Booking.all
