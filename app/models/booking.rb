a7aebc830dd58997dcaa79b605539bf238b0e05c
James McCarthy <jmccarth@uwaterloo.ca>
2013-08-12 11:26:55 -0400

    Validate that start time comes before end time. Fixes #33

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -8,11 +8,22 @@ class Booking < ActiveRecord::Base
   serialize :sign_in_times, Hash
   serialize :sign_out_times, Hash
   validate :real_user
+  validate :correct_times
   
   private
   def real_user
     #Check to see if the user already exists. If not fail the validation.
     valid = User.exists?(self.user_id)
     self.errors.add(:user, "doesn't exist. Please create the user first.") unless valid
+  end
+  
+  def correct_times
+    #Check to see that the times for the booking are sensible
+    
+    #Is the start date/time before the end date/time?
+    
+    schedule = IceCube::Schedule.from_yaml(self.schedule)
+    valid = schedule.start_time < schedule.end_time 
+    self.errors.add(:booking, "cannot be saved: Start time must come before end time") unless valid
   end
 end
