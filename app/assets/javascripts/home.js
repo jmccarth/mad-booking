242f88d822207579be6d43ecf4bc03a077cbd848
Collin McIntyre <collin.mcintyre@uwaterloo.ca>
2013-09-05 15:08:22 -0400

    further fix to relative booking urls

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 		defaultView: 'agendaWeek',
 		editable : false,
 		eventSources : [{
-			url : "bookings",
+			url : document.location + "/bookings",
 			data : {
 				start_date : startDate,
 				end_date : endDate
@@ -65,7 +65,7 @@ function reloadCalendar(items) {
 		defaultView: 'agendaWeek',
 		editable : false,
 		eventSources : [{
-			url : "bookings",
+			url : document.location + "/bookings",
 			data : {
 				start_date : startDate,
 				end_date : endDate,
