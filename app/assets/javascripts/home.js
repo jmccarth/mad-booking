5b5f807dd76e5df5436dff02d0687f9663f6addc
Collin McIntyre <collin.mcintyre@uwaterloo.ca>
2013-09-16 16:29:12 -0400

    removed duplicate fullcalendar function

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,92 +1,67 @@
 $(document).ready(function() {
-
-	var startDate = new Date() / 1000 - 31556900;
-	var endDate = new Date() / 1000 + 31556900;
-
-	var events;
-	
-	var $calendar = $("#calendar");
-
-	var calHeight = $(window).height() - $calendar.offset().top - $('footer').height();
-
-	$calendar.fullCalendar({
-		header : {
-			left : 'prev,next today',
-			center : 'title',
-			right : 'month,basicWeek,basicDay'
-		},
-		defaultView: 'basicWeek',
-		editable : false,
-		eventSources : [{
-			url : app_path + "/bookings/daterange",
-			data : {
-				start_date : startDate,
-				end_date : endDate
-			}
-		}],
-		eventClick : function(calEvent, jsEvent, view) {
-			$modalContainer = $('#modalContainer');
-			$modalContainer.foundation('reveal', 'open', {
-				url: app_path + "/bookings/" + calEvent.id + "/edit",
-				complete: function() {
-					initializeComponents($modalContainer);
-				}
-			}); 
-		},
-		eventRender : function(event, jqElement, view){
-			jqElement.find('.fc-event-title').html(event.title);
-		},
-		height: calHeight,
-		weekMode: 'liquid',
-		weekends:false,
-		slotEventOverlap:false
-
-	});
+	reloadCalendar(0);
 });
 
 function reloadCalendar(items) {
-	var startDate = new Date() / 1000 - 31556900;
-	var endDate = new Date() / 1000 + 31556900;
 
-	var $calendar = $("#calendar");
+		var startDate = new Date() / 1000 - 31556900;
+		var endDate = new Date() / 1000 + 31556900;
 
-	var calHeight = $(window).height() - $calendar.offset().top - $('footer').height();
+		var events;
+		
+		var $calendar = $("#calendar");
 
-	$calendar.fullCalendar('destroy');
+		var calHeight = $(window).height() - $calendar.offset().top - $('footer').height();
 
-	var events;
+		var params = {};
 
-	$calendar.fullCalendar({
-		header : {
-			left : 'prev,next today',
-			center : 'title',
-			right : 'month,agendaWeek,agendaDay'
-		},
-		defaultView: 'agendaWeek',
-		editable : false,
-		eventSources : [{
-			url : app_path + "/bookings/daterange",
-			data : {
+		if (items == 0) {
+			params = {
+				start_date : startDate,
+				end_date : endDate
+			}
+		}
+		else {
+			params = {
 				start_date : startDate,
 				end_date : endDate,
 				equip_ids : items
-			}
-		}],
-		eventClick : function(calEvent, jsEvent, view) {
-			$modalContainer = $('#modalContainer');
-			$modalContainer.foundation('reveal', 'open', {
-				url: app_path + "/bookings/" + calEvent.id + "/edit",
-				complete: function() {
-					initializeComponents($modalContainer);
-				}
-			}); 
-		},
-		eventRender : function(event, jqElement, view){
-			if (view.name == 'month'){
-				//jqElement.height(15);
-			}
-		},
-		height: calHeight,
-		weekMode: 'liquid'
-	});
-}
+			}	
+		}
+
+		$calendar.fullCalendar('destroy');
+
+		$calendar.fullCalendar({
+			header : {
+				left : 'prev,next today',
+				center : 'title',
+				right : 'month,basicWeek,basicDay'
+			},
+			defaultView: 'basicWeek',
+			editable : false,
+			eventSources : [{
+				url : app_path + "/bookings/daterange",
+				data : params
+			}],
+			eventClick : function(calEvent, jsEvent, view) {
+				$modalContainer = $('#modalContainer');
+				$modalContainer.foundation('reveal', 'open', {
+					url: app_path + "/bookings/" + calEvent.id + "/edit",
+					complete: function() {
+						initializeComponents($modalContainer);
+					}
+				}); 
+			},
+			eventRender : function(event, jqElement, view){
+				jqElement.find('.fc-event-title').html(event.title);
+			},
+			height: calHeight,
+			weekMode: 'liquid',
+			weekends:false,
+			slotEventOverlap:false
+
+		});
+	}
+
+
+
