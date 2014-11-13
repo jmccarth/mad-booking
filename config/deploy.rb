f9ec842d33c0dbc57018bb7af44a67a72c897dab
James McCarthy <jmccarth@uwaterloo.ca>
2014-11-13 11:02:11 -0500

    Only worry about disabling UI on _form if not a new event

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:jmccarth/mad-booking.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/home/Sites/mad-booking/'
+# set :deploy_to, '/home/Sites/mad-booking/'
 
 # Default value for :scm is :git
 # set :scm, :git
