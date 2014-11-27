ffe0dd52a80bad9dbf0191237343c6414b17a176
James McCarthy <jmccarth@uwaterloo.ca>
2014-11-27 14:45:48 -0500

    New tag model

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,10 +1,11 @@
 class Equipment < ActiveRecord::Base
   before_save :default_values
   has_and_belongs_to_many :bookings
+  has_and_belongs_to_many :tags
   belongs_to :category
   validates_presence_of :name
   validates_uniqueness_of :barcode
-  
+
   def default_values
     if self.status.nil?
       self.status = 1
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141121140612) do
+ActiveRecord::Schema.define(version: 20141127192958) do
 
   create_table "bookings", force: true do |t|
     t.text     "schedule"
@@ -47,6 +47,11 @@ ActiveRecord::Schema.define(version: 20141121140612) do
     t.string   "serial_number"
   end
 
+  create_table "equipment_tags", id: false, force: true do |t|
+    t.integer "equipment_id"
+    t.integer "tag_id"
+  end
+
   create_table "events", force: true do |t|
     t.integer  "booking_id"
     t.datetime "start"
@@ -58,6 +63,12 @@ ActiveRecord::Schema.define(version: 20141121140612) do
   create_table "settings", force: true do |t|
     t.string   "key"
     t.string   "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
