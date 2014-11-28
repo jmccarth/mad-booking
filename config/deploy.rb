267595af291e9957af134fb5d5034645d5ad2465
James McCarthy <jmccarth@uwaterloo.ca>
2014-11-28 09:08:57 -0500

    Moved javascript out of _form and into application.js.erb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'BookingSystem'
-set :repo_url, 'git@github.com:jmccarth/mad-booking.git'
+set :repo_url, 'gitlab@git.uwaterloo.ca:mad/mad-booking.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
