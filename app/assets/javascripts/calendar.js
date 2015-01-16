c1dfe9c91622caf8f34d91cbcde7df8684b09cc6
James McCarthy <jmccarth@uwaterloo.ca>
2015-01-16 15:53:28 -0500

    dropped calendar into calendar tab

---
+++ app/assets/javascripts/calendar.js
@@ -0,0 +1,39 @@
+$(document).ready(function (){
+  reloadNewCalendar();
+});
+
+function reloadNewCalendar(){
+  var $calendar = $("#newcal");
+  var $calendarLoading = $("#new-calendar-loading");
+
+  params = {};
+
+  $calendar.fullCalendar({
+    defaultView: 'basicWeek',
+    editable : false,
+    //eventClick : function(calEvent, jsEvent, view) {},
+    eventRender : function(event, jqElement, view){
+      jqElement.find('.fc-event-title').html(event.title);
+    },
+    eventSources : [{
+      url : "bookings/daterange",
+      data : params
+    }],
+    header : {
+      left : 'prev,next today',
+      center : 'title',
+      right : 'month,basicWeek,basicDay'
+    },
+    height: 750,
+    loading : function(isLoading) {
+      if (isLoading) $calendarLoading.css('opacity', '1');
+      else $calendarLoading.css('opacity', '0');
+    },
+    maxTime:19,
+    minTime:7,
+    slotEventOverlap:false,
+    timeFormat: 'h(:mm)t{ - h(:mm)t',
+    weekends:false,
+    weekMode: 'liquid'
+  });
+}
